7d283aae52b25453955d18e2d43bc4b06a50b1c5
ViacheslavKlinov <viacheslav.klinov@nure.ua>
2022-11-11 13:35:07 +0200

    Adding comments, sorting functions in a correct hierarchy

--- src/nure/WalkerRecognition/core/main.py
+++ src/nure/WalkerRecognition/core/main.py
@@ -28,31 +28,32 @@ if __name__ == '__main__':
     coordinates, test_x, test_y = prepare_test_datasets(test_images, test_names, test_labels,
                                                         window_step=step,
                                                         d_thresh=15)
-    # Подготавливаем тестовую выборку - извлекаем патчи, из патчей hog
-    # Создаём модель
+    # Creating of a model
     prob = problem(train_y, train_x)
     param = parameter('-s 3')
-    # Обучаем модель
+    # Fitting of the model
     m = train(prob, param)
     # save_model(str(SAVE_MODEL_PATH), m)
     # m = load_model(str(SAVE_MODEL_PATH))
 
-    # Валидируем модель
+    # Testing the model
     predicted_labels = []
-    # testing_indices = np.concatenate((np.arange(0, 25), np.arange(60, 80)))
     testing_indices = np.arange(0, len(test_images), 4)
     for i in testing_indices:
         p_label, p_acc, p_val = predict(test_y[i], test_x[i], m)
         predicted_labels.append(p_label)
         print(p_label, p_acc, p_val, sep='\n')
 
-
-    # Функция детектированя по меткам и координатам и отрисовки рамочки
+    image_detections = []
+    # Getting windows with detected pedestrians
     for i, j in zip(testing_indices, range(len(testing_indices))):
-        detect(predicted_labels[j], coordinates[i], test_images[i], step*3.5)
-    # Функция подсчёта точности и полноты
-    # precision, recall = composite_accuracy(test_y[testing_indices],
-    # predicted_labels, test_images[testing_indices], coordinates[testing_indices])
+        current_detections = detect(predicted_labels[j], coordinates[i], test_images[i], step*3.5)
+        show_images_with_boxes(current_detections, test_images[i])
+        image_detections.append(current_detections)
+
+    # Evaluating results
+    precision, recall = composite_accuracy(test_y[testing_indices],
+    predicted_labels, test_images[testing_indices], coordinates[testing_indices])
 
     pass
 
